2d7f13b7a592185ebd7b6ea6b62dce4594cf9acd
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2023-04-03 10:59:13 -0400

    removed redundant title and description

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', title="Bassim's Website", description="A website built with Tailwind CSS with Flask")
+    return render_template('index.html')
 
 # if __name__ == '__main__':
    # app.run(host='0.0.0.0') # for repl.it
